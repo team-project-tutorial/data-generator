f1d25cb2ea29271905a6ba84a2a4d6a2fdef6c27
choi-ys <project.log.062@gmail.com>
2025-05-22 17:20:37 +0900

    init: initial commit

---
+++ src/test/java/io/stackwise/datagenerator/DataGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.stackwise.datagenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
